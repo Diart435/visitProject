4b2a6537e5b6bd127355cfb335a76ba45d5a4fa6
Diart435 <vlad2005646@gmail.com>
2025-08-06 13:07:32 +0400

    The front

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ServiceListComponent } from './pages/service-list/service-list.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { ContactsComponent } from './pages/contacts/contacts.component';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo:'/home', pathMatch:'full'},
+  {path:'home', component:HomeComponent},
+  {path:"service", component:ServiceListComponent},
+  {path:"contacts", component:ContactsComponent},
+  {path:"**", component:NotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { ContactsComponent } from './pages/contacts/contacts.component';
+import { ServiceListComponent } from './pages/service-list/service-list.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NotFoundComponent,
+    ContactsComponent,
+    ServiceListComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
