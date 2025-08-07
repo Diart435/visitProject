42a78e09655b43b10e8e26688814820350c8522f
Diart435 <vlad2005646@gmail.com>
2025-08-07 14:22:02 +0400

    some changes

--- src/main/java/com/prod/visitBack/Controller/ServiceController.java
+++ src/main/java/com/prod/visitBack/Controller/ServiceController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = {"http://localhost:4200/", "http://electrictlt.rug"})
+@CrossOrigin(origins = {"http://localhost:4200/", "http://electrictlt.ru"})
 @RestController
 public class ServiceController {
     private final ServiceListService serviceListService;
